e0c8691e64d36a95b1891f134f11cc1bc5d68398
Alexandr Petrovich <apalion97@gmail.com>
2020-12-26 14:18:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const CURRENCY_CODE = {
+    USD: 'USD',
+    EUR: 'EUR',
+    RUB: 'RUB',
+}
+
+const getToDay = () => {
+    const date = new Date()
+
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+}
+
+const renderContent = (response) => {
+    const { data } = response;
+    let content = document.getElementById('data').innerHTML;
+
+    Object
+        .keys(data.rates)
+        .map((currencyCode) => {
+            content += `
+                <tr>
+                    <td>${data.base}</td>
+                    <td>${data.rates[currencyCode].toFixed(2)}</td>
+                </tr>
+            `;
+        });
+
+    document.getElementById('data').innerHTML = content;
+};
+
+Promise
+    .all([
+        axios.get(`https://api.ratesapi.io/api/${getToDay()}?base=${CURRENCY_CODE.USD}&symbols=${CURRENCY_CODE.RUB}`),
+        axios.get(`https://api.ratesapi.io/api/${getToDay()}?base=${CURRENCY_CODE.EUR}&symbols=${CURRENCY_CODE.RUB}`)
+    ]).then( values => values.forEach(renderContent));
